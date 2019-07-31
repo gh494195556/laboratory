0fc4583dccbdefd8c0cf3938b8833b59cdb46fc6
bei <liub@docimax.com.cn>
2019-07-31 18:03:47 +0800

    update

---
+++ 03-spa/01-vue/practice/src/components/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user">
+    <input type="text" v-model="name" @keydown.enter="handleInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user",
+  data: () => ({
+    name: ""
+  }),
+  methods: {
+    handleInput(e) {
+      this.$emit("onChange", e.target.value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+div.user {
+  border: 1px solid #000;
+  margin: 10px;
+}
+</style>
